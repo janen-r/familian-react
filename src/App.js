096b2939499169684b71d1de344fe72458529f09
Janen Rajasingh <janadev01@gmail.com>
2022-04-18 01:03:01 +0530

    Background particle animation added

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 
 import BackgroundImage from "./components/wallpaper/BackgroundImage";
 import Todos from "./components/todos/Todos";
-
 import "@material-tailwind/react/tailwind.css";
 
 function App() {
@@ -10,6 +9,10 @@ function App() {
         <div className="App">
             {/* <BackgroundImage /> */}
             <Todos />
+            {/* <link href='https://fonts.googleapis.com/css?family=Lato:300,400,700' rel='stylesheet' type='text/css'> */}
+<div id='stars'></div>
+<div id='stars2'></div>
+<div id='stars3'></div>
         </div>
     );
 }
